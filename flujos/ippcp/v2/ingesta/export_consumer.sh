51daf868826e3710b6d713bf8d077dd58cdbc6e5
JPardo08 <jpardo0824@gmail.com>
2026-08-13 12:18:15 +0200

    fix: use public DSP endpoints for IPPCP v2

--- flujos/ippcp/v2/ingesta/export_consumer.sh
+++ flujos/ippcp/v2/ingesta/export_consumer.sh
@@ -2,4 +2,4 @@
 export CONSUMER="conn-company-ippcp"
 export CONSUMER_HOST="${CONSUMER}.${DS_DOMAIN}"
 export CONSUMER_BASE="https://${CONSUMER_HOST}"
-export CONSUMER_PROTOCOL="http://${CONSUMER}:19194/protocol"
+export CONSUMER_PROTOCOL="https://${CONSUMER_HOST}/protocol"
--- flujos/ippcp/v2/ingesta/export_provider.sh
+++ flujos/ippcp/v2/ingesta/export_provider.sh
@@ -2,4 +2,4 @@
 export PROVIDER="conn-citycouncil-ippcp"
 export PROVIDER_HOST="${PROVIDER}.${DS_DOMAIN}"
 export PROVIDER_BASE="https://${PROVIDER_HOST}"
-export PROVIDER_PROTOCOL="http://${PROVIDER}:19194/protocol"
+export PROVIDER_PROTOCOL="https://${PROVIDER_HOST}/protocol"
